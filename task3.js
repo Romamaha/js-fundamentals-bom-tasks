99d476ac1daa7e5af6a5f3ee38905b32901bea6d
Roman <149516586+Romamaha@users.noreply.github.com>
2024-01-08 22:25:02 +0200

    Update task3.js

--- task3.js
+++ task3.js
@@ -3,3 +3,14 @@
 // Список з елементами знаходиться у файлі index.html
 // Для запуску використовувати Live Server
 // Для перевірки виконання виконати команду node tests/task3.test.js
+
+const deleteButton = document.getElementById("deleteButton")
+
+deleteButton.onclick = function() {
+    const selectElement = document.getElementById("dropdown")
+    const selectedOption = selectElement.options[selectElement.selectedIndex];
+
+    if(selectedOption) {
+        selectElement.removeChild(selectedOption);
+    }
+}
