f9d61d7199bcf83f8e1673b73d430cbcfae22d73
Roman <149516586+Romamaha@users.noreply.github.com>
2024-01-08 22:25:32 +0200

    Update task4.js

--- task4.js
+++ task4.js
@@ -2,3 +2,15 @@
 //  і виводитиме на поточну сторінку при її розтязі/стисканні відповідні значення.
 // Для запуску використовувати Live Server
 // Для перевірки node tests/task4.test.js
+
+function displayWindowSize() {
+    const windowWidth = innerWidth
+    const windowHeight = innerHeight
+
+    document.getElementById("widthValue").textContent = windowWidth
+    document.getElementById("heightValue").textContent = windowHeight
+
+}
+
+window.addEventListener("resize", displayWindowSize)
+displayWindowSize();
